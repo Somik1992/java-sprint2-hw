6ec5cf9fa7caa72c168867d482797de7098a3c83
Somik92 <Samsonov.work1992@yandex.ru>
2022-10-04 01:32:35 +0300

    4nd commit

--- src/YearlyReport.java
+++ src/YearlyReport.java
@@ -1,29 +1,44 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class YearlyReport {
-    static HashMap<String, ArrayList<Integer>> yearlyReport;
+    static HashMap<String, ArrayList<Integer>> yearlyReport= new HashMap<String, ArrayList<Integer>>();
     static String mask = "y.";
 
     YearlyReport() {
-        yearlyReport = new HashMap<String, ArrayList<Integer>>();
     }
 
     public static void readYerlyFile() {
         ArrayList<String> names = CustomFileReader.findAllFilesbyMask(mask);
         for (String name : names) {
             String file = CustomFileReader.readFileContentsOrNull(name);
-            String[] lines = file.split("\n");
+            String[] lines = file.split("\r\n");
+            ArrayList<Integer> report = new ArrayList<>();
+            String month = "";
             for (int i = 1; i < lines.length; i++) {
-                ArrayList<Integer> report = new ArrayList<>();
                 String[] line = lines[i].split(",");
-                String month = line[0];
                 int amount = Integer.parseInt(line[1]);
                 boolean isExpense = Boolean.parseBoolean(line[2]);
-                report.add(amount);
-                yearlyReport.put(month, report);
-                System.out.println(yearlyReport);
+
+                if (isExpense) {
+                    amount = -1 * amount;
+                }
+
+                if (Objects.equals(month, line[0]) || month.isEmpty()) {
+                    report.add(amount);
+                    month = line[0];
+                    yearlyReport.put(month, report);
+                } else {
+                    report.clear();
+                    report.add(amount);
+                    month = line[0];
+                }
+
             }
+            System.out.println(yearlyReport);
+            
         }
     }
 
