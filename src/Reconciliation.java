c7fbb963236c44f25cabbd0105f094a9e81c9cba
igor <samsonov.work1992@yandex.ru>
2022-10-08 03:12:11 +0300

    reconcilReport() - done

--- src/Reconciliation.java
+++ src/Reconciliation.java
@@ -1,6 +1,43 @@
-public class Reconciliation {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class Reconciliation {
+    static HashMap<String, Integer> reconcileYearlyReport= new HashMap<String, Integer>();
+    static HashMap<String, Integer> reconcileMountlyReport= new HashMap<String, Integer>();
     public static void reconcilReport() {
-        System.out.printf("Hi");
+
+
+        for (String month: YearlyReport.yearlyReport.keySet()) {
+            int resultYearlyReport = 0;
+            for (Integer value : YearlyReport.yearlyReport.get(month)) {
+                resultYearlyReport += value;
+                reconcileYearlyReport.put(month, resultYearlyReport);
+            }
+        }
+
+        for (String month: MonthlyReport.TotalReport.keySet()) {
+            int resultMountlyReport = 0;
+            HashMap<String, ArrayList<Integer>> expenses = MonthlyReport.TotalReport.get(month);
+            for (String value : expenses.keySet()) {
+                resultMountlyReport += expenses.get(value).get(0);
+                reconcileMountlyReport.put(month, resultMountlyReport);
+            }
+        }
+        if (reconcileYearlyReport.equals(reconcileMountlyReport)) {
+            System.out.println("Реконсиляция прошла успешно, данные вверны");
+        } else {
+            if (reconcileYearlyReport.size() == reconcileMountlyReport.size()) {
+                for (String mounth : reconcileMountlyReport.keySet()) {
+                    if (!reconcileMountlyReport.get(mounth).equals(reconcileYearlyReport.get(mounth))) {
+                        System.out.println("Ошибка реконсиляции");
+                        System.out.println("Проблема в месяце " + mounth);
+                    }
+                }
+            } else {
+                System.out.println("Количество месяцев в годовых отчетах и количество месяцев в месячных отчетах " +
+                        "не равно, скорее всего вы не загрузили какой-то отчет");
+            }
+
+        }
     }
 }
